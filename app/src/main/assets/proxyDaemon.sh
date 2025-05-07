db97dd4fdffafae6dba5f5a46e024bd48677327a
oasis <shengoasis@gmail.com>
2025-05-07 11:46:59 +0800

    v1.1.4 refactor： 1. 脚本小修改

--- app/src/main/assets/proxyDaemon.sh
+++ app/src/main/assets/proxyDaemon.sh
@@ -5,10 +5,6 @@ tun='tun0'             # 虚拟接口
 dev='wlan0'            # 物理接口
 interval=3             # 检测间隔
 pref=18000             # 策略路由优先级
-log_file="/data/local/tmp/proxy_log.txt"
-
-# 进程标识（方便 grep 检测）
-echo "proxy.sh running with PID $$" >> "$log_file"
 
 # 开启 IP 转发
 sysctl -w net.ipv4.ip_forward=1
@@ -24,6 +20,12 @@ iptables -t nat -A POSTROUTING -o $tun -j MASQUERADE
 ip rule add from all table main pref $pref
 ip rule add from all iif $dev table $tun pref $(expr $pref - 1)
 
+# 允许局域网发往代理（$dev -> $tun）
+iptables -A FORWARD -i $dev -o $tun -j ACCEPT
+
+# 允许代理回包到局域网（$tun -> $dev）
+iptables -A FORWARD -i $tun -o $dev -m state --state ESTABLISHED,RELATED -j ACCEPT
+
 contain="from all iif $dev lookup $tun"
 
 # 无限循环检测网络状态并恢复策略
@@ -37,10 +39,10 @@ while true; do
 
     if [ "$rule_ok" -ne 0 ]; then
         if [ "$dev_up" -ne 0 ]; then
-            echo "[$now] $dev is down" >> "$log_file"
+            echo "[$now] $dev is down"
         else
             ip rule add from all iif $dev table $tun pref $(expr $pref - 1)
-            echo "[$now] Reset routing policy for $dev" >> "$log_file"
+            echo "[$now] Reset routing policy for $dev"
         fi
     fi
     sleep "$interval"
